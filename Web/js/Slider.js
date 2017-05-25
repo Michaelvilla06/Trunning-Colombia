48dcca6764bcc988d64b946a657e9bbb10c035dd
Michaelvilla06 <maicolvillafc@gmail.com>
2017-05-24 23:17:02 -0500

    Trunning-Colombia

---
+++ Web/js/Slider.js
@@ -0,0 +1,13 @@
+		$(function(){
+  $(".slides").slidesjs({
+    play: {
+      active: true,
+      effect: "slide",
+      interval: 3000,
+      auto: true,
+      swap: true,
+      pauseOnHover: false,
+      restartDelay: 2500
+    }
+  });
+});
